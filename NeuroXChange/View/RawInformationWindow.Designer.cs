c52e982111d74173a8c418a0f81ed4035dd53b9a
UladzimirSukharukau <valdeming@gmail.com>
2017-10-19 22:23:00 +0100

    Fix raw window resizing

--- NeuroXChange/View/RawInformationWindow.Designer.cs
+++ NeuroXChange/View/RawInformationWindow.Designer.cs
@@ -32,21 +32,29 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.bioDataRTB = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // heartRateRTB
             // 
+            this.heartRateRTB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.heartRateRTB.BackColor = System.Drawing.SystemColors.Control;
-            this.heartRateRTB.Location = new System.Drawing.Point(5, 226);
+            this.heartRateRTB.Location = new System.Drawing.Point(6, 18);
             this.heartRateRTB.Name = "heartRateRTB";
-            this.heartRateRTB.Size = new System.Drawing.Size(278, 60);
+            this.heartRateRTB.Size = new System.Drawing.Size(281, 100);
             this.heartRateRTB.TabIndex = 7;
             this.heartRateRTB.Text = "";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2, 210);
+            this.label2.Location = new System.Drawing.Point(3, 2);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(107, 13);
             this.label2.TabIndex = 6;
@@ -55,7 +63,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(2, 2);
+            this.label1.Location = new System.Drawing.Point(3, 4);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 5;
@@ -63,29 +71,54 @@
             // 
             // bioDataRTB
             // 
-            this.bioDataRTB.Location = new System.Drawing.Point(5, 16);
+            this.bioDataRTB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bioDataRTB.Location = new System.Drawing.Point(6, 20);
             this.bioDataRTB.Name = "bioDataRTB";
             this.bioDataRTB.ReadOnly = true;
-            this.bioDataRTB.Size = new System.Drawing.Size(278, 191);
+            this.bioDataRTB.Size = new System.Drawing.Size(281, 196);
             this.bioDataRTB.TabIndex = 4;
             this.bioDataRTB.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.bioDataRTB);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.heartRateRTB);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(293, 346);
+            this.splitContainer1.SplitterDistance = 219;
+            this.splitContainer1.TabIndex = 8;
             // 
             // RawInformationWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 292);
-            this.Controls.Add(this.heartRateRTB);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.bioDataRTB);
+            this.ClientSize = new System.Drawing.Size(293, 346);
+            this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "RawInformationWindow";
             this.Text = "Raw information";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RawInformationWindow_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,5 +128,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.RichTextBox bioDataRTB;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
