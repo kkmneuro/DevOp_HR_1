f278aea08cb3b82f98f9d2d054c9688a4664bcc8
UladzimirSukharukau <valdeming@gmail.com>
2018-01-18 22:59:49 +0000

    First version of StaticAnalysisTool

---
+++ PostTradingAnalysis/MainWindow.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PostTradingAnalysis
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.mainChart = new LiveCharts.WinForms.CartesianChart();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbFile
+            // 
+            this.tbFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFile.Location = new System.Drawing.Point(61, 7);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(785, 20);
+            this.tbFile.TabIndex = 0;
+            this.tbFile.Text = "C:\\tmp\\neurotrader\\PsychophysiologyDatabase.mdb";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFile.Location = new System.Drawing.Point(849, 6);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(24, 22);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Database:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Controls.Add(this.dtpTo);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dtpFrom);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbFile);
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(943, 59);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(879, 5);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(58, 48);
+            this.btnLoad.TabIndex = 7;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.CustomFormat = "dd/MM/yyyy   HH:mm:ss";
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTo.Location = new System.Drawing.Point(247, 33);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(147, 20);
+            this.dtpTo.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "To:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "From:";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.CustomFormat = "dd/MM/yyyy   HH:mm:ss";
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFrom.Location = new System.Drawing.Point(61, 33);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(147, 20);
+            this.dtpFrom.TabIndex = 3;
+            this.dtpFrom.Value = new System.DateTime(2018, 1, 7, 21, 17, 0, 0);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "MS Access files|*.mdb";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.mainChart);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(935, 357);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Charts";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // mainChart
+            // 
+            this.mainChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainChart.Location = new System.Drawing.Point(3, 3);
+            this.mainChart.Name = "mainChart";
+            this.mainChart.Size = new System.Drawing.Size(929, 351);
+            this.mainChart.TabIndex = 0;
+            this.mainChart.Text = "cartesianChart1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 59);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(943, 383);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(935, 357);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Statistics";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 442);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainWindow";
+            this.Text = "Post trading analysis";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private LiveCharts.WinForms.CartesianChart mainChart;
+    }
+}
+
---
+++ PostTradingAnalysis/MainWindow.cs
@@ -0,0 +1,143 @@
+﻿using NeuroXChange.Model.BioData;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+using LiveCharts; //Core of the library
+using LiveCharts.Wpf; //The WPF controls
+using LiveCharts.WinForms; //the WinForm wrappers
+using System.Windows.Media;
+using LiveCharts.Configurations;
+
+namespace PostTradingAnalysis
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                tbFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = "Data";
+
+            var now = DateTime.Now;
+            var timeFrom = new DateTime(now.Year, now.Month, now.Day);
+            var timeTo = new DateTime(now.Year, now.Month, now.Day, 23, 59, 59);
+
+            //dtpFrom.Value = timeFrom;
+            dtpTo.Value = timeTo;
+
+            btnLoad_Click(null, null);
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        ChartValues<BioData> bioData = new ChartValues<BioData>();
+
+        private void LoadData()
+        {
+            var fileName = tbFile.Text;
+            if (!File.Exists(fileName))
+            {
+                throw new Exception("There are no such file: \"" + fileName + "\"");
+            }
+
+            var dateFrom = dtpFrom.Value;
+            var dateTo = dtpTo.Value;
+            if (dateFrom >= dateTo)
+            {
+                throw new Exception("First date should be less than second date!");
+            }
+
+            bioData.Clear();
+
+            var connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"{0}\"", fileName);
+            var connection = new OleDbConnection(connectionString);
+            connection.Open();
+            var cmd = new OleDbCommand();
+            cmd.Connection = connection;
+
+            // biodata Loading
+            var biodataSQL = string.Format(
+                @"SELECT BioData.*, sellPrice, buyPrice From BioData
+            LEFT OUTER JOIN PriceAtBioDataTick ON BioData.ID = PriceAtBioDataTick.ID
+            WHERE BioData.Time BETWEEN #{0:MM/dd/yyyy HH:mm:ss}# AND #{1:MM/dd/yyyy HH:mm:ss}#
+            ORDER BY BioData.ID",
+                dateFrom, dateTo);
+            cmd.CommandText = biodataSQL;
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var data = BioData.FromOleDbDataReader(reader, true);
+                //if (bioData.Count < 1000)
+                {
+                    bioData.Add(data);
+                }
+            }
+
+            var hrValues = bioData;
+
+            //var hrValues = new ChartValues<double>();
+
+            //// biodata charts
+            //foreach (var bioDataPoint in bioData)
+            //{
+            //    hrValues.Add(bioDataPoint.heartRate);
+            //    if(hrValues.Count >= 2000)
+            //    {
+            //        //break;
+            //    }
+            //}
+
+            var mapper = Mappers.Xy<BioData>()
+                .X(point => point.time.ToOADate())
+                .Y(point => point.heartRate);
+
+            var hrSeries = new LineSeries(mapper) {
+                Title = "Heart rate",
+                Values = hrValues
+            };
+            hrSeries.LineSmoothness = 0;
+
+            hrSeries.PointGeometry = null;
+            hrSeries.StrokeThickness = 1;
+            hrSeries.Fill = Brushes.Transparent;
+            
+            mainChart.DisableAnimations = true;
+            mainChart.DataTooltip = null;
+            mainChart.Pan = PanningOptions.X;
+            mainChart.Zoom = ZoomingOptions.X;
+            mainChart.Hoverable = false;
+            mainChart.AxisX.Add(new LiveCharts.Wpf.Axis
+            {
+                LabelFormatter = value => DateTime.FromOADate(value).ToString("MM/dd HH:mm:ss")
+            });
+
+            var collection = new LiveCharts.SeriesCollection { hrSeries };
+            mainChart.Series = collection;
+        }
+    }
+}
