8dcd1a6d84a8758c9f3e252307b838446cdb61c9
UladzimirSukharukau <valdeming@gmail.com>
2018-03-12 23:33:32 +0000

    Simplify NeuroTraderServer and configure it to work on Azure VM

--- NeuroTraderServer/Program.cs
+++ NeuroTraderServer/Program.cs
@@ -7,19 +7,6 @@ using System.Linq;
 using System.Data.SqlClient;
 using global::NeuroTraderServer.Properties;
 
-// State object for reading client data asynchronously  
-public class StateObject
-{
-    // Client  socket.  
-    public Socket workSocket = null;
-    // Size of receive buffer.  
-    public const int BufferSize = 1024;
-    // Receive buffer.  
-    public byte[] buffer = new byte[BufferSize];
-    // Received data string.  
-    public StringBuilder sb = new StringBuilder();
-}
-
 public static class AsynchronousSocketListener
 {
     public static char[] credentialsSeparators = {'\t', '\v'};
@@ -46,14 +33,12 @@ public static class AsynchronousSocketListener
 
     public static void StartListening()
     {
-        // Data buffer for incoming data.  
-        byte[] bytes = new Byte[1024];
-
         // Establish the local endpoint for the socket.  
         // The DNS name of the computer  
-        // running the listener is "host.contoso.com".  
-        //var dnsGetHostName = Dns.GetHostName();
-        var dnsGetHostName = "localhost";
+ 
+        var dnsGetHostName = Dns.GetHostName();
+        //var dnsGetHostName = "localhost";
+
         Console.WriteLine($"Dns.GetHostName: '{dnsGetHostName}'");
         IPHostEntry ipHostInfo = Dns.GetHostEntry(dnsGetHostName);
         IPAddress ipAddress = ipHostInfo.AddressList.First(address => address.AddressFamily == AddressFamily.InterNetwork);
@@ -105,112 +90,65 @@ public static class AsynchronousSocketListener
 
         // Get the socket that handles the client request.  
         Socket listener = (Socket)ar.AsyncState;
-        Socket handler = listener.EndAccept(ar);
-
-        // Create the state object.  
-        StateObject state = new StateObject();
-        state.workSocket = handler;
-        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-            new AsyncCallback(ReadCallback), state);
-    }
-
-    public static void ReadCallback(IAsyncResult ar)
-    {
-        String content = String.Empty;
-
-        // Retrieve the state object and the handler socket  
-        // from the asynchronous state object.  
-        StateObject state = (StateObject)ar.AsyncState;
-        Socket handler = state.workSocket;
-
-        // Read data from the client socket.   
-        int bytesRead = handler.EndReceive(ar);
-
-        if (bytesRead > 0)
+        using (Socket handler = listener.EndAccept(ar))
         {
-            // There  might be more data, so store the data received so far.  
-            state.sb.Append(Encoding.ASCII.GetString(
-                state.buffer, 0, bytesRead));
-
-            // Check for end-of-file tag. If it is not there, read   
-            // more data.  
-            content = state.sb.ToString();
-            if (content.IndexOf("\v") > -1)
+            string content = string.Empty;
+            while (content.IndexOf("\v") < 0)
             {
-                // All the data has been read from the   
-                // client. Display it on the console.  
-                //Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
-                //    content.Length, content);
-                // Echo the data back to the client.  
-                //Send(handler, content);
-                var credentials = content.Split(credentialsSeparators);
-                Console.WriteLine($"Received credentials: {string.Join(" : ", credentials)}");
-                var resultMsg = new byte[1];
-                resultMsg[0] = 200;
-                if (credentials.Count() < 2)
+                var bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                content += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            }
+
+            var credentials = content.Split(credentialsSeparators);
+            Console.WriteLine($"\tReceived credentials: {string.Join(" : ", credentials)}");
+            var resultMsg = new byte[1];
+            resultMsg[0] = 200;
+            if (credentials.Count() < 2)
+            {
+                resultMsg[0] = 201;
+            }
+            else
+            {
+                using (var command = new SqlCommand($"SELECT * FROM dbo.Users WHERE [Login] = '{credentials[0]}'", connection))
                 {
-                    resultMsg[0] = 201;
-                }
-                else
-                {
-                    using (var command = new SqlCommand($"SELECT * FROM dbo.Users WHERE [Login] = '{credentials[0]}'", connection))
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        if (!reader.Read())
                         {
-                            if (reader.Read())
+                            resultMsg[0] = 202;
+                        }
+                        else
+                        {
+                            var correctPassword = reader["Password"].ToString();
+                            if (credentials[1] != correctPassword)
                             {
-                                var correctPassword = reader["Password"].ToString();
-                                if(credentials[1] != correctPassword)
-                                {
-                                    resultMsg[0] = 203;
-                                }
-                            }
-                            else
-                            {
-                                resultMsg[0] = 202;
+                                resultMsg[0] = 203;
                             }
                         }
                     }
                 }
-                handler.Send(resultMsg);
             }
-            else
+
+            switch (resultMsg[0])
             {
-                // Not all data received. Get more.  
-                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
+                case 200:
+                    Console.WriteLine("\tAuthorised");
+                    break;
+                case 201:
+                    Console.WriteLine("\tError in the protocol");
+                    break;
+                case 202:
+                    Console.WriteLine("\tNo such user");
+                    break;
+                case 203:
+                    Console.WriteLine("\tWrong password");
+                    break;
             }
-        }
-    }
 
-    private static void Send(Socket handler, String data)
-    {
-        // Convert the string data to byte data using ASCII encoding.  
-        byte[] byteData = Encoding.ASCII.GetBytes(data);
+            handler.Send(resultMsg);
 
-        // Begin sending the data to the remote device.  
-        handler.BeginSend(byteData, 0, byteData.Length, 0,
-            new AsyncCallback(SendCallback), handler);
-    }
-
-    private static void SendCallback(IAsyncResult ar)
-    {
-        try
-        {
-            // Retrieve the socket from the state object.  
-            Socket handler = (Socket)ar.AsyncState;
-
-            // Complete sending the data to the remote device.  
-            int bytesSent = handler.EndSend(ar);
-            Console.WriteLine("Sent {0} bytes to client.", bytesSent);
-
-            handler.Shutdown(SocketShutdown.Both);
             handler.Close();
-
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.ToString());
         }
     }
 
