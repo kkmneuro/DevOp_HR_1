087517212dc313ce99bbbfc924293a3b65459792
Neurotrader <32505165+kkmneuro@users.noreply.github.com>
2019-12-01 21:52:23 +0100

    Add files via upload

---
+++ HR_Builds/ZokiAllFormsNeuroXChange/Common/IniFileReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.IO;
+
+namespace NeuroXChange.Common
+{
+    public class IniFileReader
+    {
+        private string Path;
+        private string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFileReader(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+        }
+
+        public string Read(string Key, string Section, string DefaultValue = "" )
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section, Key, DefaultValue, RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section)
+        {
+            WritePrivateProfileString(Section, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
---
+++ HR_Builds/ZokiAllFormsNeuroXChange/Common/StringHelpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace NeuroXChange.Common
+{
+    public static class StringHelpers
+    {
+        public static double ParseDoubleCultureIndependent(string value, bool allowSign = false)
+        {
+            double result;
+
+            NumberStyles style = NumberStyles.AllowDecimalPoint;
+            if (allowSign)
+            {
+                style |= NumberStyles.AllowLeadingSign;
+            }
+
+            //Try parsing in the current culture
+            if (double.TryParse(value, style, CultureInfo.CurrentCulture, out result) ||
+                //Then in neutral language
+                double.TryParse(value, style, CultureInfo.InvariantCulture, out result) ||
+                //Then in french language
+                double.TryParse(value, style, CultureInfo.GetCultureInfo("fr-FR"), out result))
+            {
+                return result;
+            }
+
+            throw new Exception("Can't parse float value of the string!");
+        }
+
+        public static bool TryParseDoubleCultureIndependent(string value, out double result)
+        {
+            result = 0.0;
+            try
+            {
+                result = ParseDoubleCultureIndependent(value);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string NullableToString<T> (T? a) where T : struct
+        {
+            return a.HasValue ? a.ToString() : "NULL";
+        }
+    }
+}
